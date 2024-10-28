19b23503570e298e9a6e2c46150527bc74da8af2
rivenos <imanechk16@gmail.com>
2024-10-28 09:28:49 +0100

    commit 1 : initialisation du projet

---
+++ Employeemngt/src/test/java/com/example/Employeemngt/EmployeemngtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Employeemngt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemngtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
